afe9d3973a4be4f52a87b0d68488a12c025d0543
Manuel Figueira <manuel.figueira.9@gmail.com>
2026-05-13 15:12:40 -0300

    update env.ts

--- env.ts
+++ env.ts
@@ -1,56 +1,45 @@
-import { env as loadEnv } from 'custom-env'
-import { z } from 'zod'
+import dotenv from 'dotenv';
+import { z } from 'zod';
 
-process.env.APP_STAGE = process.env.APP_STAGE || 'dev'
+process.env.APP_STAGE = process.env.APP_STAGE || 'development';
 
-const isProduction = process.env.APP_STAGE === 'production'
-const isDevelopment = process.env.APP_STAGE === 'dev'
-const isTesting = process.env.APP_STAGE === 'test'
+export const isDevelopment = process.env.APP_STAGE === 'development';
+export const isTesting = process.env.APP_STAGE === 'test';
+export const isProduction = process.env.APP_STAGE === 'production';
 
 if (isDevelopment) {
-  loadEnv()
+  dotenv.config({ path: ['.env.local', '.env'] });
 } else if (isTesting) {
-  loadEnv('test')
+  dotenv.config({ path: '.env.test' });
 }
 
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
-
-  APP_STAGE: z.enum(['dev', 'test', 'production']).default('dev'),
+  APP_STAGE: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
 
   PORT: z.coerce.number().positive().default(3000),
   DATABASE_URL: z.string().startsWith('postgresql://'),
   JWT_SECRET: z.string().min(32, 'Must be 32 chars long'),
   JWT_EXPIRES_IN: z.string().default('7d'),
   BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
-})
+});
 
-export type Env = z.infer<typeof envSchema>
-let env: Env
+export type Env = z.infer<typeof envSchema>;
 
-try {
-  env = envSchema.parse(process.env)
-} catch (e) {
-  if (e instanceof z.ZodError) {
-    console.log('Invalid env var')
-    console.error(JSON.stringify(e.flatten().fieldErrors, null, 2))
+let env: Env;
 
-    e.issues.forEach((err) => {
-      const path = err.path.join('.')
-      console.log(`${path}: ${err.message}`)
-    })
+const parsedEnv = envSchema.safeParse(process.env);
 
-    process.exit(1)
-  }
-
-  throw e
+if (!parsedEnv.success) {
+  console.error('Invalid ENV variables:\n');
+  console.error(JSON.stringify(z.treeifyError(parsedEnv.error), null, 2));
+  process.exit(1);
 }
 
-export const isProd = () => env.APP_STAGE === 'production'
-export const isDev = () => env.APP_STAGE === 'dev'
-export const isTest = () => env.APP_STAGE === 'test'
+env = parsedEnv.data;
 
-export { env }
-export default env
+export default env;
